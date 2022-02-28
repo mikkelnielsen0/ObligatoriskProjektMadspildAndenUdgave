1378487536ec0e546e2622d7d65a4d1ea28af5f8
mikkel <mikk017y@stud.kea.dk>
2022-02-28 13:53:28 +0100

    Initial commit

---
+++ src/test/java/com/example/obligatoriskprojektmadspild/ObligatoriskProjektMadspildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatoriskprojektmadspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatoriskProjektMadspildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
